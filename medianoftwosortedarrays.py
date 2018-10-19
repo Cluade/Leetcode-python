39ab3f7c093e52f065514b703f8daa12b2475c46
Duan Yiqun <duanyiquncc@gmail.com>
2018-10-19 16:29:01 -0700

    fix

--- medianoftwosortedarrays.py
+++ medianoftwosortedarrays.py
@@ -19,18 +19,20 @@ class Solution:
             return self.findMedianSortedArrays(nums2,nums1)
         while cut1<=n1:
             cut1 = int((cr-cl)/2+cl)
+            print(cut1)
             cut2 = int(N/2 -cut1)
+            print(cut2)
             L1 = min_int if cut1 ==0 else nums1[cut1-1]
-            L2 = min_int if cut2 ==0 else nums1[cut2-1]
-            R1 = max_int if cut1 ==0 else nums1[cut1]
-            R2 = max_int if cut2 ==0 else nums2[cut2]
+            L2 = min_int if cut2 ==0 else nums2[cut2-1]
+            R1 = max_int if cut1 ==n1 else nums1[cut1]
+            R2 = max_int if cut2 ==n2 else nums2[cut2]
             
             if L1 > R2 :
                 cr = cut1 -1
-            elif L2> R1:
+            elif L2 > R1:
                 cl = cut1 +1
             else:
-                if N%2 ==0:
+                if N%2 == 0:
                     L1 = max(L1, L2)
                     R1 = min(R1, R2)
                     return (L1+R1)/2
